df37ecc85762194e525a0f4bbb8a15838a48d10d
Thomas Boutell <tom@apostrophecms.com>
2021-11-09 07:07:57 -0500

    days in the future

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ a pixel display. Uses the openweathermap API and the jimp library.
 
 You'll need to create a `config.json` file with `latitude`, `longitude` and `apiKey` properties. The `apiKey` must be an api key registered with openweathermap.
 
-When you run the application `weather.png` is updated in the same folder.
+When you run the application `weather.png` is updated in the same folder, unless you pass `--filename`.
+
+You can pass `--days=n` to produce a png for n days in the future.
--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import fetch from 'node-fetch';
 import fs from 'fs';
 import path from 'path';
 import Jimp from 'jimp';
+import boring from 'boring';
+
+const argv = boring();
 
 const __dirname = path.resolve();
 
@@ -11,7 +14,12 @@ go();
 
 async function go() {
   const data = await get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`, 'json');
-  const { temp, weather } = data.daily[0];
+  const days = argv.days ? parseInt(argv.days) : 0;
+  const { temp, weather } = data.daily[days];
+  console.log(data.daily[days]);
+  const date = new Date();
+  date.setDate(date.getDate() + days);
+  const dayOfWeek = [ 'Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa' ][date.getDay()];
   let { min, max } = temp;
   min = fahrenheit(min);
   max = fahrenheit(max);
@@ -20,11 +28,12 @@ async function go() {
   const iconImage = await Jimp.read(iconUrl);
   await iconImage.resize(32, 32);
   const image = await new Jimp(64, 32, 'black');
-  image.blit(iconImage, 32, 0);
+  image.blit(iconImage, 16, 0);
   const font = await Jimp.loadFont(Jimp.FONT_SANS_16_WHITE);
   await image.print(font, 0, 0, max);
   await image.print(font, 0, image.bitmap.height - 16, min);
-  await image.write('weather.png');
+  await image.print(font, 48, 8, dayOfWeek);
+  await image.write(argv.filename || 'weather.png');
 }
 
 function fahrenheit(kelvin) {
